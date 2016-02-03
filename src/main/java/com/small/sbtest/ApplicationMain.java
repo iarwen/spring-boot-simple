722101f35cadf56d572d1aeafae8c480bc062dd4
changwentao <changwenwen@126.com>
2016-02-03 11:42:48 +0800

    a simple spring boot example

---
+++ src/main/java/com/small/sbtest/ApplicationMain.java
@@ -0,0 +1,17 @@
+package com.small.sbtest;
+
+  
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+ 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class ApplicationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class);
+    }
+}
+
---
+++ src/main/java/com/small/sbtest/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.small.sbtest.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.small.sbtest.domain.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping("/{id}")
+	public User view(@PathVariable("id") Long id) {
+		User user = new User();
+		user.setId(id);
+		user.setName("zhang");
+		return user;
+	}
+
+}
