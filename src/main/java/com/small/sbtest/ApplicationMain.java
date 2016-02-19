e25940350254138053640bfdc5d936b1cd104b11
changwentao <changwenwen@126.com>
2016-02-19 11:18:01 +0800

    format

--- src/main/java/com/small/sbtest/ApplicationMain.java
+++ src/main/java/com/small/sbtest/ApplicationMain.java
@@ -1,17 +1,15 @@
 package com.small.sbtest;
 
-  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
- 
+
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class ApplicationMain {
-    public static void main(String[] args) {
-        SpringApplication.run(ApplicationMain.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMain.class);
+	}
 }
-
